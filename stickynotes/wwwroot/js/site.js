74148e6afd890f8df668c5f5ba82d4cf4f7c29d0
jundachen10 <jundachen10@gmail.com>
2023-06-08 19:17:22 -0400

    added css and html

--- stickynotes/wwwroot/js/site.js
+++ stickynotes/wwwroot/js/site.js
@@ -1,5 +1,26 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', function () {
+    const stickyNotes = document.getElementsByClassName('stickynote');
 
+    function resizeText() {
+        for (let i = 0; i < stickyNotes.length; i++) {
+            const stickyNote = stickyNotes[i];
+            const text = stickyNote.querySelector('p');
+
+            let fontSize = 12; // Set an initial font size in pt
+            text.style.fontSize = fontSize + 'pt';
+
+            while (
+                text.scrollHeight > stickyNote.offsetHeight ||
+                text.scrollWidth > stickyNote.offsetWidth
+            ) {
+                fontSize -= 1;
+                text.style.fontSize = fontSize + 'pt';
+            }
+        }
+    }
+
+    resizeText();
+});
